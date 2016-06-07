b17c327b7626ddbaddc4949b8aee4b1fc3f95b27
Andrew Bergan <andrew.c.bergan@nasa.gov>
2016-06-07 14:08:57 -0400

    Added install_requires to setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ setup(
 	name="abaverify",
 	version="0.1dev",
 	packages=["abaverify",],
+	install_requires=["jsonparser",],
 	dependency_links=["ssh://fe.larc.nasa.gov/scr2/git/jsonparser.git/tarball/master#egg=jsonparser"]
 )
