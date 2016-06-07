79412915a056db44749cad16ead73d3f06e50ade
Andrew Bergan <andrew.c.bergan@nasa.gov>
2016-06-07 15:51:42 -0400

    Added jsonparser back in

---
+++ abaverify/jsonparser.py
@@ -0,0 +1,34 @@
+import json
+import re
+
+# From: http://www.lifl.fr/~damien.riquet/parse-a-json-file-with-comments.html
+
+# Regular expression for comments
+comment_re = re.compile(
+    '(^)?[^\S\n]*/(?:\*(.*?)\*/[^\S\n]*|/[^\n]*)($)?',
+    re.DOTALL | re.MULTILINE
+)
+
+def parse(filename):
+    """ Parse a JSON file
+        First remove comments and then use the json module package
+        Comments look like :
+            // ...
+        or
+            /*
+            ...
+            */
+    """
+    with open(filename) as f:
+        content = ''.join(f.readlines())
+
+        ## Looking for comments
+        match = comment_re.search(content)
+        while match:
+            # single line comment
+            content = content[:match.start()] + content[match.end():]
+            match = comment_re.search(content)
+        
+
+        # Return json file
+        return json.loads(content)
